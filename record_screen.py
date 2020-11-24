6b980dc7d706da166bb6aae0a96cae8589a2bb71
ikabanen <74080976+ikabanen@users.noreply.github.com>
2020-11-23 16:14:49 -0800

    Update record_screen.py

--- record_screen.py
+++ record_screen.py
@@ -12,17 +12,21 @@ out=cv2.VideoWriter(filename,fourcc,framerate,(screen_size))
 
 Xs = [0,6,2,5,4,2,2,0]
 Ys = [0,2,2,4,5,2,6,0]
-while True:
-    img = pyautogui.screenshot()
-    mouseX,mouseY = pyautogui.position()
-    img_np=np.array(img)
-    frame=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+try:
+    while True:
+        img = pyautogui.screenshot()
+        mouseX,mouseY = pyautogui.position()
+        img_np=np.array(img)
+        frame=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
 
-    Xthis=[4 * x + mouseX for x in Xs]
-    Ythis=[4 * y + mouseY for y in Ys]
-    points=list(zip(Xthis, Ythis))
-    points=np.array(points, 'int32')
-    cv2.fillPoly(frame, [points], color=[1, 190, 200])
-    out.write(frame)
+        Xthis=[4 * x + mouseX for x in Xs]
+        Ythis=[4 * y + mouseY for y in Ys]
+        points=list(zip(Xthis, Ythis))
+        points=np.array(points, 'int32')
+        cv2.fillPoly(frame, [points], color=[1, 190, 200])
+        out.write(frame)
+
+except KeyboardInterrupt: # end program by pressing Ctrl-C
+    pass
 
 out.release()
